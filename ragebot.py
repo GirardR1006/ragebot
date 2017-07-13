f405cf4f921c813bd50ea36b58c4d54b2bea19e8
GirardR1006 <ichumonji@gmail.com>
2017-07-13 11:16:33 +0100

    Initial commit

---
+++ ragebot.py
@@ -0,0 +1,21 @@
+import os
+from slackclient import SlackClient
+
+
+BOT_NAME = 'ragebot'
+
+slack_client = SlackClient(os.environ.get('SLACKBOT_TOKEN'))
+
+
+if __name__ == "__main__":
+    api_call = slack_client.api_call("users.list")
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        users = api_call.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') == BOT_NAME:
+                print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
+            else:
+                print("ID for '" + user['name'] + "' is " + user.get('id'))
+    else:
+        print("failed to connect, Slackbot token is " + os.environ.get('SLACKBOT_TOKEN'))
