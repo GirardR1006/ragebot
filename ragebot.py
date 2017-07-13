51819d640463c8c6fb9dc5287bea0ab38ede1a67
GirardR1006 <ichumonji@gmail.com>
2017-07-13 13:37:27 +0100

    Bot is now ready to express pure rage

--- ragebot.py
+++ ragebot.py
@@ -1,21 +1,43 @@
 import os
+import time
 from slackclient import SlackClient
 
+#ID and token as environment token
+BOT_TOKEN=os.environ.get('SLACKBOT_TOKEN')
 
-BOT_NAME = 'ragebot'
+#Instantiate slackbot client
+slack_client = SlackClient(BOT_TOKEN)
 
-slack_client = SlackClient(os.environ.get('SLACKBOT_TOKEN'))
+def express_pure_rage(CHANNEL, MESSAGE):
+    """
+        RECEIVES MESSAGE FROM CHANNEL, CHECK IF IT'S UPPERCASE. IF NOT, 
+        WILL YELL AT USER.
+    """        
+    if not MESSAGE.isupper():
+        RESPONSE = "ICI, ON ÉCRIT EN MAJUSCULE! TU DEVRAIS ÉCRIRE: " + MESSAGE.upper() 
+        slack_client.api_call("chat.postMessage", channel=CHANNEL, text=RESPONSE, as_user=True)
+
+
+def parse_slack_output(slack_rtm_output):
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output:
+                # return text, whitespace removed
+                return output['text'], output['channel']
+    return None, None
 
 
 if __name__ == "__main__":
-    api_call = slack_client.api_call("users.list")
-    if api_call.get('ok'):
-        # retrieve all users so we can find our bot
-        users = api_call.get('members')
-        for user in users:
-            if 'name' in user and user.get('name') == BOT_NAME:
-                print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
-            else:
-                print("ID for '" + user['name'] + "' is " + user.get('id'))
+    READ_WEBSOCKET_DELAY = 1 # 1 SECOND DELAY BETWEEN READINGS 
+    if slack_client.rtm_connect():
+        print("RAGEBOT IS CONNECTED! REEEEEEEEEE")
+        while True:
+            MESSAGE, CHANNEL = parse_slack_output(slack_client.rtm_read())
+            #THE HECK WITH STRING BEING BOOLEAN"""
+            if MESSAGE and CHANNEL:
+                express_pure_rage(CHANNEL, MESSAGE)
+            time.sleep(READ_WEBSOCKET_DELAY)
     else:
-        print("failed to connect, Slackbot token is " + os.environ.get('SLACKBOT_TOKEN'))
+        print("CONNECTION FAILED! CHECK YOUR SLACKBOT TOKEN YOU DAMNED NORMIE!")
+
